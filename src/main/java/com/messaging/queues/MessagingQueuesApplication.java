7e673fb1e0722a555a0fd6d87b31e9ad8308265e
Darshan Shah <shahdarshah1510@gmail.com>
2020-05-23 17:14:38 +0530

    Initializing the project

---
+++ src/main/java/com/messaging/queues/MessagingQueuesApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingQueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingQueuesApplication.class, args);
+	}
+
+}
